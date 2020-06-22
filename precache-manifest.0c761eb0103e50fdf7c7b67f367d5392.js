2c29456993c4203995ebc94015ab909133574d22
Glib Rulev <velzepooz@gmail.com>
2020-06-22 16:10:20 +0300

    Updates

---
+++ precache-manifest.0c761eb0103e50fdf7c7b67f367d5392.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "237e1438f74afe42cfe16a855c7ff2ed",
+    "url": "./index.html"
+  },
+  {
+    "revision": "27442e2390f880eeda11",
+    "url": "./static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "36689209a43d29ff1e8c",
+    "url": "./static/js/2.d4c7f416.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.d4c7f416.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27442e2390f880eeda11",
+    "url": "./static/js/main.12d756e9.chunk.js"
+  },
+  {
+    "revision": "ec438ca3dae444460a4d",
+    "url": "./static/js/runtime-main.1d48480d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.56475be9cba24000b69e470ba1f5d46c.js"
+  "./precache-manifest.0c761eb0103e50fdf7c7b67f367d5392.js"
 );
 
 self.addEventListener('message', (event) => {
